82efc2082bebbb6ab667eb3ea974d6e652d8e6b4
Alvaro Schmit <alvaroschmit@gmail.com>
2022-01-26 22:03:41 -0300

    subiendo a github

--- js/cotizador/data.js
+++ js/cotizador/data.js
@@ -1,36 +1,77 @@
 //SSSSSSEEEEEELLLLLEEEEECCCCTTTTTTTTTTTTTTTTTTTTTTTTTTT
 
-let dataSelect = [{
+let reboqueDataSelect = [{
+    'id':'idreboque00',
     'class': 'reboqueEnduido',
     'InfoCampo': 'Reboque'
 }, {
+    'id':'idreboque01',
     'class': 'reboqueEnduido',
     'InfoCampo': 'Enduido'
 }, {
+    'id':'idreboque02',
     'class': 'reboqueEnduido',
     'InfoCampo': 'Madera'
 }, {
+    'id':'idreboque03',
     'class': 'reboqueEnduido',
     'InfoCampo': 'Metal'
 },{
-    'class': 'madera',
+    'id':'idreboque04',
+    'class': 'reboqueEnduido',
     'InfoCampo': 'Reboque'
 }, {
-    'class': 'madera',
+    'id':'idreboque05',
+    'class': 'reboqueEnduido',
     'InfoCampo': 'Enduido'
-}, {
-    'class': 'madera',
-    'InfoCampo': 'Madera'
-}, {
-    'class': 'metal',
-    'InfoCampo': 'Metal'
-}, {
-    'class': 'metal',
-    'InfoCampo': 'Metal'
 }
 ]
 
 
+let maderaDataSelect = [{
+    'id':'idMadera00',
+    'class': 'madera',
+    'InfoCampo': 'Tipo de madera'
+}, {'id':'idMadera01',
+    'class': 'madera',
+    'InfoCampo': 'Estado de la Superficie'
+}, {'id':'idMadera02',
+    'class': 'madera',
+    'InfoCampo': 'terminacion actual'
+}, {'id':'idMadera03',
+    'class': 'madera',
+    'InfoCampo': 'Terminacion a aplicar'
+},{'id':'idMadera04',
+    'class': 'madera',
+    'InfoCampo': 'otra pregunta'
+},{'id':'idMadera05',
+'class': 'madera',
+'InfoCampo': 'otra pregunta'
+}
+]
+
+
+let metalDataSelect = [{
+    'id':'idMetal00',
+    'class': 'metal',
+    'InfoCampo': 'Tipo de madera'
+}, {'id':'idMetal01',
+    'class': 'metal',
+    'InfoCampo': 'Estado de la Superficie'
+}, {'id':'idMetal02',
+    'class': 'metal',
+    'InfoCampo': 'terminacion actual'
+}, {'id':'idMetal03',
+    'class': 'metal',
+    'InfoCampo': 'Terminacion a aplicar'
+},{'id':'idMetal04',
+    'class': 'metal',
+    'InfoCampo': 'otra pregunta'
+},{'id':'idMetal05',
+'class': 'metal',
+'InfoCampo': 'otra pregunta'
+}
+]
 //SSSSSSEEEEEELLLLLEEEEECCCCTTTTTTTTTTTTTTTTTTTTTTTTTTT
 
 ////////////////////////////////////////////////////
@@ -39,7 +80,7 @@ let dataSelect = [{
 //000000000000000000000000000000000000000000000000000
 
 
-let dataOption00 = [{
+let reboqueDataOpt00 = [{
     'valorMultiplicador': '1',
     'textOption': 'Reboque'
 }, {
@@ -57,7 +98,102 @@ let dataOption00 = [{
 //1111111111111111111111111111111111111111111111111
 
 
-let dataOption01 = [{
+let reboqueDataOpt01 = [{
+    'valorMultiplicador': '1',
+    'textOption': 'madera'
+}, {
+    'valorMultiplicador': '0.8',
+    'textOption': 'Enduido'
+}, {
+    'valorMultiplicador': '1.6',
+    'textOption': 'Madera'
+}, {
+    'valorMultiplicador': '1.7',
+    'textOption': 'Metal'
+},
+]
+
+//222222222222222222222222222222222222222222222
+
+let reboqueDataOpt02 = [{
+    'valorMultiplicador': '0.9',
+    'textOption': 'Optimas condiciones'
+}, {
+    'valorMultiplicador': '1.3',
+    'textOption': 'Con detalles y pintura descascarada'
+}, {
+    'valorMultiplicador': '1.5',
+    'textOption': 'Muy deteriorado'
+}
+]
+
+//33333333333333333333333333333333333333333333333333333333
+
+let reboqueDataOpt03 = [{
+    'valorMultiplicador': '1',
+    'textOption': 'Blanco'
+}, {
+    'valorMultiplicador': '1.1',
+    'textOption': 'Color claro'
+}, {
+    'valorMultiplicador': '1.3',
+    'textOption': 'Color Oscuro'
+}
+]
+
+
+//444444444444444444444444444444444444444444444444444444
+
+let reboqueDataOpt04 = [{
+    'valorMultiplicador': '1',
+    'textOption': 'Blanco'
+}, {
+    'valorMultiplicador': '1.1',
+    'textOption': 'Color claro'
+}, {
+    'valorMultiplicador': '1.3',
+    'textOption': 'Color Oscuro'
+}
+]
+
+//5555555555555555555555555555555555555555555555555555
+
+
+let reboqueDataOpt05 = [{
+    'valorMultiplicador': '1',
+    'textOption': 'Opcion01'
+}, {
+    'valorMultiplicador': '1',
+    'textOption': 'Opcion02'
+}, {
+    'valorMultiplicador': '1',
+    'textOption': 'Opcion03'
+}, {
+    'valorMultiplicador': '1',
+    'textOption': 'Opcion04'
+}
+]
+
+//MMMMMADDDEEEERRRAAAAAAAAAAAAAAAAAAAAAAAAAA
+//000000000000000000000000000000000000000000000000000
+
+
+let maderaDataOpt00 = [{
+    'valorMultiplicador': '0.9',
+    'textOption': 'madera Lisa'
+}, {
+    'valorMultiplicador': '1',
+    'textOption': ' madera Textura fina'
+}, {
+    'valorMultiplicador': '1.5',
+    'textOption': 'madera Textura gruesa'
+},
+]
+
+//1111111111111111111111111111111111111111111111111
+
+
+let maderaDataOpt01 = [{
     'valorMultiplicador': '0.9',
     'textOption': 'Lisa'
 }, {
@@ -74,7 +210,7 @@ let dataOption01 = [{
 
 //222222222222222222222222222222222222222222222
 
-let dataOption02 = [{
+let maderaDataOpt02 = [{
     'valorMultiplicador': '0.9',
     'textOption': 'Optimas condiciones'
 }, {
@@ -88,7 +224,7 @@ let dataOption02 = [{
 
 //33333333333333333333333333333333333333333333333333333333
 
-let dataOption03 = [{
+let maderaDataOpt03 = [{
     'valorMultiplicador': '1',
     'textOption': 'Blanco'
 }, {
@@ -103,7 +239,7 @@ let dataOption03 = [{
 
 //444444444444444444444444444444444444444444444444444444
 
-let dataOption04 = [{
+let maderaDataOpt04 = [{
     'valorMultiplicador': '1',
     'textOption': 'Blanco'
 }, {
@@ -118,44 +254,7 @@ let dataOption04 = [{
 //5555555555555555555555555555555555555555555555555555
 
 
-let dataOption05 = [{
-    'valorMultiplicador': '1',
-    'textOption': 'Opcion01'
-}, {
-    'valorMultiplicador': '1',
-    'textOption': 'Opcion02'
-}, {
-    'valorMultiplicador': '1',
-    'textOption': 'Opcion03'
-}, {
-    'valorMultiplicador': '1',
-    'textOption': 'Opcion04'
-}
-]
-
-//666666666666666666666666666666666666666666666666
-
-
-let dataOption06 = [{
-    'valorMultiplicador': '1',
-    'textOption': 'prueba'
-}, {
-    'valorMultiplicador': '1',
-    'textOption': 'Opcion02'
-}, {
-    'valorMultiplicador': '1',
-    'textOption': 'Opcion03'
-}, {
-    'valorMultiplicador': '1',
-    'textOption': 'Opcion04'
-}
-]
-
-
-// //77777777777777777777777777777777777777777777777777777777
-
-
-let dataOption07 = [{
+let maderaDataOpt05 = [{
     'valorMultiplicador': '1',
     'textOption': 'Opcion01'
 }, {
@@ -171,11 +270,91 @@ let dataOption07 = [{
 ]
 
 
-// //88888888888888888888888888888888888888888888888888888
+//mmmmmmmmmmmmmm
+//eeeeeeeeeeeeee
+//tttttttttttttt
+//aaaaaaaaaaaaaa
+//llllllllllllll
+//000000000000000000000000000000000000000000000000000
 
 
+let metalDataOpt00 = [{
+    'valorMultiplicador': '0.9',
+    'textOption': 'madera Lisa'
+}, {
+    'valorMultiplicador': '1',
+    'textOption': ' madera Textura fina'
+}, {
+    'valorMultiplicador': '1.5',
+    'textOption': 'madera Textura gruesa'
+},
+]
 
-let dataOption08 = [{
+//1111111111111111111111111111111111111111111111111
+
+
+let metalDataOpt01 = [{
+    'valorMultiplicador': '0.9',
+    'textOption': 'Lisa'
+}, {
+    'valorMultiplicador': '1',
+    'textOption': 'Textura fina'
+}, {
+    'valorMultiplicador': '1.5',
+    'textOption': 'Textura gruesa'
+}, {
+    'valorMultiplicador': '2',
+    'textOption': 'Ladrillo visto'
+},
+]
+
+//222222222222222222222222222222222222222222222
+
+let metalDataOpt02 = [{
+    'valorMultiplicador': '0.9',
+    'textOption': 'Optimas condiciones'
+}, {
+    'valorMultiplicador': '1.3',
+    'textOption': 'Con detalles y pintura descascarada'
+}, {
+    'valorMultiplicador': '1.5',
+    'textOption': 'Muy deteriorado'
+}
+]
+
+//33333333333333333333333333333333333333333333333333333333
+
+let metalDataOpt03 = [{
+    'valorMultiplicador': '1',
+    'textOption': 'Blanco'
+}, {
+    'valorMultiplicador': '1.1',
+    'textOption': 'Color claro'
+}, {
+    'valorMultiplicador': '1.3',
+    'textOption': 'Color Oscuro'
+}
+]
+
+
+//444444444444444444444444444444444444444444444444444444
+
+let metalDataOpt04 = [{
+    'valorMultiplicador': '1',
+    'textOption': 'Blanco'
+}, {
+    'valorMultiplicador': '1.1',
+    'textOption': 'Color claro'
+}, {
+    'valorMultiplicador': '1.3',
+    'textOption': 'Color Oscuro'
+}
+]
+
+//5555555555555555555555555555555555555555555555555555
+
+
+let metalDataOpt05 = [{
     'valorMultiplicador': '1',
     'textOption': 'Opcion01'
 }, {
@@ -189,4 +368,3 @@ let dataOption08 = [{
     'textOption': 'Opcion04'
 }
 ]
-
--- js/cotizador/logica.js
+++ js/cotizador/logica.js
@@ -2,78 +2,154 @@
 
 let selectContainer = document.getElementById('selectContainer')
 
-function loadSelect() {
-    dataSelect.forEach((data2,i) => {
+function loadSelect(dataSelect) {
+    let carga = true
+    if (carga !=true) {
+        console.log('no carga');
+    } else {
+        dataSelect.forEach((data2) => {
 
-        selectContainer.innerHTML += `<div class="${data2.class}" ><p>${data2.InfoCampo}</p><select id="select0${i}" class="select"></select></div>`
-    });
+            selectContainer.innerHTML += `<div class="${data2.class}" ><p>${data2.InfoCampo}</p><select id="${data2.id}" class="select"></select></div>`
+        });  
+    }
+   
 }
-loadSelect()
+
 
 //funcion que carga etiquetas option
+function getElement() {
+let idreboque00 = document.getElementById('idreboque00');
+let idreboque01 = document.getElementById('idreboque01');
+let idreboque02 = document.getElementById('idreboque02');
+let idreboque03 = document.getElementById('idreboque03');
+let idreboque04 = document.getElementById('idreboque04');
+let idreboque05 = document.getElementById('idreboque05');
+}
 
-let select00 = document.getElementById('select00')
-let select01 = document.getElementById('select01')
-let select02 = document.getElementById('select02')
-let select03 = document.getElementById('select03')
-let select04 = document.getElementById('select04')
-let select05 = document.getElementById('select05')
-let select06 = document.getElementById('select06')
-let select07 = document.getElementById('select07')
-let select08 = document.getElementById('select08')
+function getElementMadera() {
+    let idMadera00 = document.getElementById('idMadera00');
+    let idMadera01 = document.getElementById('idMadera01');
+    let idMadera02 = document.getElementById('idMadera02');
+    let idMadera03 = document.getElementById('idMadera03');
+    let idMadera04 = document.getElementById('idMadera04');
+    let idMadera05 = document.getElementById('idMadera05');
+    }
+    function getElementMetal() {
+        let idMetal00 = document.getElementById('idMetal00');
+        let idMetal01 = document.getElementById('idMetal01');
+        let idMetal02 = document.getElementById('idMetal02');
+        let idMetal03 = document.getElementById('idMetal03');
+        let idMetal04 = document.getElementById('idMetal04');
+        let idMetal05 = document.getElementById('idMetal05');
+        }
 
 function loadOptions(select0X, dataOption0X) {
     dataOption0X.forEach(data => {
     select0X.innerHTML +=`<option value="${data.valorMultiplicador}">${data.textOption}</option>`;
     });
 }
-loadOptions(select00,dataOption00);
-loadOptions(select01,dataOption01);
-loadOptions(select02,dataOption02);
-loadOptions(select03,dataOption03);
-loadOptions(select04,dataOption04);
-loadOptions(select05,dataOption05);
-loadOptions(select06,dataOption06);
-loadOptions(select07,dataOption07);
-loadOptions(select08,dataOption08);
+
+
+function toReboqueEnduido() {
+    loadOptions(idreboque00,reboqueDataOpt00);
+    loadOptions(idreboque01,reboqueDataOpt01);
+    loadOptions(idreboque02,reboqueDataOpt02);
+    loadOptions(idreboque03,reboqueDataOpt03);
+    loadOptions(idreboque04,reboqueDataOpt04);
+    loadOptions(idreboque05,reboqueDataOpt05);
+}
+
+
+function toMadera() {
+    loadOptions(idMadera00,maderaDataOpt00);
+    loadOptions(idMadera01,maderaDataOpt01);
+    loadOptions(idMadera02,maderaDataOpt02);
+    loadOptions(idMadera03,maderaDataOpt03);
+    loadOptions(idMadera04,maderaDataOpt04);
+    loadOptions(idMadera05,maderaDataOpt05);
+}
+
+function toMetal() {
+    loadOptions(idMetal00,MetalDataOpt00);
+    loadOptions(idMetal01,MetalDataOpt01);
+    loadOptions(idMetal02,MetalDataOpt02);
+    loadOptions(idMetal03,MetalDataOpt03);
+    loadOptions(idMetal04,MetalDataOpt04);
+    loadOptions(idMetal05,MetalDataOpt05);
+}
+
 
 //DIV contenedores de select con display none para iniciarlos no visibles
 
-let reboqueEnduido = document.querySelectorAll('.reboqueEnduido');
-let madera = document.querySelectorAll('.madera');
-let metal = document.querySelectorAll('.metal');
-
+// let reboqueEnduido = document.querySelectorAll('.reboqueEnduido');
+// let madera = document.querySelectorAll('.madera');
+// let metal = document.querySelectorAll('.metal');
+// let despliegueMenu = document.getElementById('despliegueMenu')
  
  
- reboqueEnduido.forEach(element =>  element.style.display = "none");
- madera.forEach(element =>  element.style.display = "none");
- metal.forEach(element =>  element.style.display = "none");
+//  reboqueEnduido.forEach(element =>  element.style.display = "none");
+//  madera.forEach(element =>  element.style.display = "none");
+//  metal.forEach(element =>  element.style.display = "none");
 
 
 // funcion que hace aparecer los select segun la eleccion de materiales
 
 
-selectMaterial.addEventListener('change', selectData0= ()=> {
 
-if (selectMaterial.value == 'reboque') {
+
+
+selectMaterial.addEventListener('change', selectData= ()=> {
+
+  
    
- reboqueEnduido.forEach(element =>  element.style.display = "block");
+if (selectMaterial.value == 'reboque') {
+    let madera = document.querySelectorAll('.madera');
+    madera.forEach(element =>  element.style.display = "none");
+
+
+    loadSelect(reboqueDataSelect);
+   getElement()
+   toReboqueEnduido();
+   eventoChange()
+   
 }
+ else if (selectMaterial.value == 'madera') {
+   let reboqueEnduido = document.querySelectorAll('.reboqueEnduido');
     
+ reboqueEnduido.forEach(element =>  element.style.display = "none");
+ loadSelect(maderaDataSelect);
+ getElementMadera()
+   toMadera();
+  
+  
+}
+else if (selectMaterial.value == 'metal') {
+    let reboqueEnduido = document.querySelectorAll('.reboqueEnduido');
+     
+  reboqueEnduido.forEach(element =>  element.style.display = "none");
+  loadSelect(metalDataSelect);
+  getElementMetal()
+    toMetal();
+   
+   
+ }
+
   
   });
 
 // funcion evento change de los select
+function eventoChange() {
+    
 
-select00.addEventListener('change', selectData0= ()=> select00.value);
-select01.addEventListener('change', selectData1= ()=> select01.value) ;
-select02.addEventListener('change', selectData2= ()=> select02.value) ;
-select03.addEventListener('change', selectData3= ()=> select03.value) ;
-select04.addEventListener('change', selectData4= ()=> select04.value) ;
-select05.addEventListener('change', selectData5= ()=> select05.value) ;
-select05.addEventListener('change', selectData5= ()=> select06.value) ;
+idreboque00.addEventListener('change', idreboqueData0= ()=> idreboque00.value);
+idreboque01.addEventListener('change', idreboqueData1= ()=> idreboque01.value) ;
+idreboque02.addEventListener('change', idreboqueData2= ()=> idreboque02.value) ;
+idreboque03.addEventListener('change', idreboqueData3= ()=> idreboque03.value) ;
+idreboque04.addEventListener('change', idreboqueData4= ()=> idreboque04.value) ;
+idreboque05.addEventListener('change', idreboqueData5= ()=> idreboque05.value) ;
+}
 
-//evento click de cotizar
+// evento click de cotizar
 
 let metrosIngresados = document.getElementById('metrosIngresados')
 function metros() {
@@ -89,7 +165,7 @@ let resultadoEnPantalla = document.getElementById('resultadoEnPantalla')// resul
 
 function cotizar() {
    let precioXmetro = 100
-    let resultado = `${precioXmetro}` *`${metros()}` * `${selectData0()}`* `${selectData1()}`* `${selectData2()}`* `${selectData3()}`* `${selectData4()}`* `${selectData5()}`//aca tengo que sumar los otros valores
+    let resultado = `${precioXmetro}` *`${metros()}` * `${idreboqueData0()}`* `${idreboqueData1()}`* `${idreboqueData2()}`* `${idreboqueData3()}`* `${idreboqueData4()}`* `${idreboqueData5()}`//aca tengo que sumar los otros valores
     console.log(resultado);
   
 resultadoEnPantalla.innerHTML= `El resultado de la cotización segun los valores ingresados es : $ ${resultado}`
